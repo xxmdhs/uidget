4f2947cbc10d0fbf539c1f5c50bd75b6d9acf6e1
xmdhs <xmdhss@gmail.com>
2020-04-15 17:35:04 +0800

    add somethings

--- src/top/xmdhs/uidget/a.java
+++ src/top/xmdhs/uidget/a.java
@@ -10,9 +10,9 @@ import java.util.logging.Logger;
 public class a {
     public static void main(String[] args) {
         papapa[] pa = new papapa[3];
-        pa[0] = new papapa("2147483646", "1", "1075816");
-        pa[1] = new papapa("2147483645", "1075817", "2151632");
-        pa[2] = new papapa("2147483644", "2151633", "3227448");
+        pa[0] = new papapa("2147483643", "1", "1075816");
+        pa[1] = new papapa("2147483642", "1075817", "2151632");
+        pa[2] = new papapa("2147483641", "2151633", "3227448");
         ScheduledThreadPoolExecutor exec = new ScheduledThreadPoolExecutor(5);
         exec.scheduleAtFixedRate(() -> pa[0].run(), 1000, 500, TimeUnit.MILLISECONDS);
         exec.scheduleAtFixedRate(() -> pa[1].run(), 2000, 500, TimeUnit.MILLISECONDS);
@@ -46,7 +46,7 @@ class papapa {
             InputStream in = papapa.class.getResourceAsStream("/logging.properties");
             logManager.readConfiguration(in);
             logManager.addLogger(logger);
-            if (i <= Integer.parseInt(end)) {
+            if (i <= Integer.parseInt(end) || s.getUid(Integer.toString(i)) == -1) {
                 i = s.getUid(uid);
                 URL url = new URL("https://www.mcbbs.net/api/mobile/index.php?module=profile&uid=" + i);
                 http h = new http(url);
@@ -56,7 +56,7 @@ class papapa {
                 } else {
                     if (h.json2Class(h.getJson()) == null) {
                         if(h.getJson().contains("messageval")){
-                        logger.warning("此用户大概有什么问题，uid：" + i);
+                            System.out.println("此用户大概有什么问题，uid：" + i);
                         }else {
                             logger.warning("网络似乎有什么问题");
                             Thread.sleep(10000);
@@ -64,7 +64,7 @@ class papapa {
                     } else {
                         uidapi u = h.json2Class(h.getJson());
                         String username = u.Variables.space.username.replace("'", "''");
-                        System.out.println("用户名：" + username + "，uid：" + u.Variables.space.uid);
+                        logger.warning("漏掉的用户，用户名：" + username + "，uid：" + u.Variables.space.uid);
                         if (u.Variables.space.group != null) {
                             s.insertsql(u.Variables.space.uid, username, u.Variables.space.credits, u.Variables.space.extcredits1,
                                     u.Variables.space.extcredits2, u.Variables.space.extcredits3, u.Variables.space.extcredits4,
@@ -80,6 +80,9 @@ class papapa {
                     }
                     s.setUid(i, uid);
                 }
+            }else {
+                i++;
+                s.setUid(i,uid);
             }
         } catch (Exception e) {
             e.printStackTrace();
